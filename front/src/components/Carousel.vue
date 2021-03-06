1bcaa08422ed9309766929c289dfdc9c7fe890d0
Samborus <p.felczak@gmail.com>
2021-03-06 23:07:43 +0100

    imgs

---
+++ front/src/components/Carousel.vue
@@ -0,0 +1,41 @@
+<template>
+      <b-carousel
+      id="carousel-1"
+      :interval="0"
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333 !important;"
+    >
+      <!-- Text slides with image -->
+      <slot name="main"></slot>
+      <b-carousel-slide 
+        img-src="https://picsum.photos/1024/480/?image=52">
+      </b-carousel-slide>
+      <!-- 
+      <b-carousel-slide 
+        img-src="https://picsum.photos/1024/480/?image=52">
+      </b-carousel-slide> -->
+  </b-carousel>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+.carousel-caption {
+    position: absolute !important;
+    right: 0 !important;
+    bottom: 0 !important;
+    left: 0 !important;
+    z-index: 10 !important;
+    padding-top: 0 !important;
+    padding-bottom: 0 !important;
+    color: #fff !important;
+    text-align: center !important;
+    width: 100% !important;
+    height: 100% !important;
+}
+</style>
---
+++ front/src/components/SlideImg.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+    <img :src="pics[index]" class="slider-left" />
+    <img :src="pics[index + 1]" class="slider-right" />
+</div>
+</template>
+<script>
+export default {
+    props: {
+        pics: [],
+        index: null
+    },
+}
+</script>
+<style scoped>
+.slider-left , .slider-right {
+    position: absolute;
+  background-size: cover;
+  background-position: center;   
+} 
+.slider-left {
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    -webkit-clip-path: polygon(0 0, 90% 0, 80% 100%, 0% 100%);
+    clip-path: polygon(0 0, 90% 0, 80% 100%, 0% 100%);
+}
+
+.slider-right {
+    left: 90%;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    /* -webkit-clip-path: polygon(0 0, 90% 0, 80% 100%, 0% 100%);
+    clip-path: polygon(0 0, 90% 0, 80% 100%, 0% 100%); */
+} 
+
+.carousel-caption {
+    position: absolute !important;
+    right: 0 !important;
+    bottom: 0 !important;
+    left: 0 !important;
+    z-index: 10 !important;
+    padding-top: 0 !important;
+    padding-bottom: 0 !important;
+    color: #fff !important;
+    text-align: center !important;
+    width: 100% !important;
+    height: 100% !important;
+}
+</style>
